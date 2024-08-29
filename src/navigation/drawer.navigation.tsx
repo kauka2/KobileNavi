c88c7d2403ac93c2af1b8fc1f8f90769796a7128
kauka2 <barbosak2526@gmail.com>
2024-08-29 08:27:14 -0300

    catch

--- src/navigation/drawer.navigation.tsx
+++ src/navigation/drawer.navigation.tsx
@@ -1,11 +1,14 @@
 import { createDrawerNavigator, DrawerNavigationProp } from '@react-navigation/drawer';
 import { colors } from "../styles/colors";
-import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import { TabNavigation } from "./tab.navigation";
 import { Camera } from '../screens/Camera';
+import { Imagens } from '../screens/Imagens';
+
 type DrawerParamList = {
     Tab: undefined
     Camera: undefined
+    Imagem: undefined
 }
 type DrawerScreenNavigationProp = DrawerNavigationProp<DrawerParamList, 'Tab'>
 export type DrawerTypes = {
@@ -30,13 +33,19 @@ export function DrawerNavigation() {
                 drawerIcon: () => (
                     <Ionicons name="person" size={24} color={colors.white} />
                 ),
-            }}/>
-                <Drawer.Screen name='Camera' component={Camera}
-                options={{
-                    drawerIcon: () => (
-                        <Ionicons name="camera" size={24} color={colors.white} />
-                    ),
-                }} />
+        }}/>
+            <Drawer.Screen name='Camera' component={Camera}
+            options={{
+                drawerIcon: () => (
+                    <Ionicons name="camera" size={24} color={colors.white} />
+                ),
+        }} />
+            <Drawer.Screen name='Imagem' component={Imagens}
+            options={{
+                drawerIcon: () => (
+                    <FontAwesome name="picture-o" size={24} color={colors.white} />
+                ),
+        }} />
         </Drawer.Navigator>
     )
 }
